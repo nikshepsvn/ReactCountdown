88c978bcb43cde930b6505f49b3b161ac9a06b54
nikshepsvn <nikshepsvn@gmail.com>
2017-10-09 02:28:37 -0400

    basic render on DOM

--- reactcountdown/src/index.js
+++ reactcountdown/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <div>React CountDown</div>, document.getElementById('root')
+);
