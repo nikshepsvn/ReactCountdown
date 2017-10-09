1033d920befad52cfc2928119c3059b4a7d3a1bb
nikshepsvn <nikshepsvn@gmail.com>
2017-10-09 03:55:03 -0400

    created a clock componenet

--- reactcountdown/src/App.jsx
+++ reactcountdown/src/App.jsx
@@ -1,30 +1,29 @@
 import React, {Component} from 'react';
+import Clock from './Clock';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      deadline: "November 25, 2017"
+      deadline: "November 25, 2017",
+      newDeadline: ""
     }
   }
 
   changeDeadline(){
-    this.setState({deadline: "October 25. 2017"})
+    this.setState({deadline:this.state.newDeadline});
   }
 
   render(){
     return(
       <div className = "App">
-        <div className = "App-title"> Countdown to {this.state.deadline}</div>
-        <div>
-          <div className = "clockDays">14 Days</div>
-          <div className = "clockHours">35 Hours</div>
-          <div className = "clockMinutes">30 Minutes</div>
-          <div className = "clockSeconds">14 Seconds</div>
+        <div className = "App-title"> Countdown to {this.state.deadline}
         </div>
+        <Clock />
         <div>
-          <input placeholder = "new date" />
+          <input placeholder = "new date"
+          onChange = {event => {this.setState({newDeadline: event.target.value})}}/>
           <button onClick = {() => {this.changeDeadline()}}> Submit </button>
         </div>
       </div>
---
+++ reactcountdown/src/Clock.jsx
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class Clock extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      days:0,
+      hours:0,
+      minutes:0,
+      seconds:0
+    }
+  }
+  
+  render(){
+    return (
+      <div>
+        <div className = "clockDays">{this.state.days} Days</div>
+        <div className = "clockHours">{this.state.hours} Hours</div>
+        <div className = "clockMinutes">{this.state.minutes} Minutes</div>
+        <div className = "clockSeconds">{this.state.seconds} Seconds</div>
+      </div>
+    )
+  }
+}
+
+export default Clock;
